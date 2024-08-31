a8fe9076c9363e70e1ab18beacc66c6ed97fc9c0
gurudesh0801 <gurudesh2204@gmail.com>
2024-09-01 00:40:37 +0530

    new changes

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -19,11 +19,6 @@ export const Contact = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(name, email, message);
-
-    {
-      /* replace below with your own Service ID, Template ID and Public Key from your EmailJS account */
-    }
-
     emailjs
       .sendForm(
         "YOUR_SERVICE_ID",
